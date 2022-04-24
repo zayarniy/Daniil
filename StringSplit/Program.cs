91f8a5da937be07924a9ae2c544cebd369cab611
Андрей Заярный <zaazaa@yandex.ru>
2022-04-24 12:40:36 +0300

    Array string

---
+++ StringSplit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReadLn(a,b,c);10 20 30 Pascal
+            string s=Console.ReadLine();
+            string[] ss = s.Split(" ");
+            for(int i=0;i<ss.Length;i++)
+                Console.WriteLine(ss[i]);
+            int[] a = new int[ss.Length];
+            for (int i = 0; i < ss.Length; i++)
+                a[i] = Convert.ToInt32(ss[i]);
+
+            //int[] b = Array.ConvertAll<string, int>(ss, new Converter<string, int>(Convert.ToInt32));
+
+        }
+    }
+}
